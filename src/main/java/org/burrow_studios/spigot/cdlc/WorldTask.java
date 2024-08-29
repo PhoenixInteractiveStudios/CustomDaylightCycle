f92ffe8170d9dc9d19d686a81faf7d5005d02598
TurtleException <turtle.exception@eldritch.de>
2024-08-29 16:32:36 +0200

    Fix time calculation

--- src/main/java/org/burrow_studios/spigot/cdlc/WorldTask.java
+++ src/main/java/org/burrow_studios/spigot/cdlc/WorldTask.java
@@ -33,14 +33,15 @@ public class WorldTask extends BukkitRunnable {
         }
 
         // don't allow tick if the counter hasn't reached 1 yet
-        if (counterDay < 1.0)
+        if (counterDay > 0.0 && counterDay < 1.0)
             this.world.setFullTime(this.world.getFullTime() - 1);
 
         // jump ahead if counterDay is still greater than 1
-        int jumpTicks = (int) counterDay;
-        counterDay += jumpTicks;
-        if (jumpTicks > 0)
+        if (counterDay > 1.0) {
+            int jumpTicks = (int) counterDay;
+            counterDay -= jumpTicks;
             this.world.setFullTime(this.world.getFullTime() + jumpTicks);
+        }
 
         // increase counter
         counterDay += factorDay;
@@ -53,21 +54,22 @@ public class WorldTask extends BukkitRunnable {
         }
 
         // don't allow tick if the counter hasn't reached 1 yet
-        if (counterNight < 1.0)
+        if (counterNight > 0.0 && counterNight < 1.0)
             this.world.setFullTime(this.world.getFullTime() - 1);
 
         // jump ahead if counterDay is still greater than 1
-        int jumpTicks = (int) counterNight;
-        counterNight += jumpTicks;
-        if (jumpTicks > 0)
+        if (counterNight > 1.0) {
+            int jumpTicks = (int) counterNight;
+            counterNight -= jumpTicks;
             this.world.setFullTime(this.world.getFullTime() + jumpTicks);
+        }
 
         // increase counter
         counterNight += factorNight;
     }
 
     private boolean isDay() {
-        return this.world.getFullTime() < 12000;
+        return this.world.getFullTime() % 24000 < 12000;
     }
 
     public @NotNull String getWorldName() {
